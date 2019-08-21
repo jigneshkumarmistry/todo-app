8575a0408fc9c1c2b16b0fae3bc1dad7bc61144f
jigneshmistry <jignesh.mistry@thegatewaycorp.com>
2019-08-21 12:04:49 +0530

    #1. Updated get method in todoController.js now it will fetch record based on id, user type and all list for admin #2. Added update and delete method into todoController.js #3. Added new routes for get, update and delete method in todoRoutes.js

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,14 +1,39 @@
 import { TodoModel } from '../models';
 
+// For creating new todos
 function create(data, userId) {
     return TodoModel.create({ ...data, userId });
 }
 
-function getAll() {
-    return TodoModel.find();
+// For get todos with mulitple user role and single and list 
+function get(id, user) {
+    console.log(user);
+    if (id) {
+        //For id specific
+        return TodoModel.findById(id);
+    } else if (user.isAdmin === false) {
+        //For normal user
+       let userId = user._id;
+        return TodoModel.find({ userId });
+    } else {
+        // For Admin 
+        return TodoModel.find();
+    }
+}
+
+// For update todos
+function update(id, data) {
+    return TodoModel.findByIdAndUpdate({ _id:id }, data, { new : true } );
+}
+
+// For delete todos
+function remove(id) {
+    return TodoModel.findByIdAndDelete(id);
 }
 
 export {
     create,
-    getAll
+    get,
+    update,
+    remove
 }
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -5,9 +5,11 @@ import { todoController } from '../controllers';
 const routes = express.Router();
 
 routes.all('/todos', verifyToken);
+
+// For all records
 routes.get('/todos', async (req, res, next) => {
     try {
-        const records = await todoController.getAll();
+        const records = await todoController.get(undefined, req.decoded.data);
         res.status(200).json({ success: true, records });
     }
     catch (error) {
@@ -16,11 +18,46 @@ routes.get('/todos', async (req, res, next) => {
     }
 });
 
+// For single record
+routes.get('/todos/:id', async (req, res, next) => {
+    try {
+        const record = await todoController.get(req.params.id);
+        res.status(200).json({ success: true, record });
+    } catch (error) {
+        console.log(error.message);
+        next(error);
+    }
+});
+
+// For new todos
 routes.post('/todos', async (req, res, next) => {
     try {
         const record = await todoController.create(req.body, req.decoded.data._id);
         res.status(201).json({ success: true, record });
     } catch (error) {
+        console.log(error.message);
+        next(error);
+    }
+});
+
+// For Update todos
+routes.put('/todos/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const record = await todoController.update(id, req.body);
+        res.status(200).json({ success: true, record });
+    } catch (error) {
+        console.log(error.message);
+        next(error);
+    }
+});
+
+// For Delete Todos
+routes.delete('/todos/:id', async (req, res, next) => {
+    try {
+        const record = await todoController.remove(req.params.id);
+        res.status(200).json({ success: true, method: req.method, path: req.path });  
+    } catch(error) {
         console.log(error.message);
         next(error);
     }
